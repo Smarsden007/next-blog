278630c7cead6fdd13fcf38e0fc83ecad633d444
Smarsden007 <seannmarsden@gmail.com>
2022-08-28 08:51:09 -0700

    Working on components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing'},
-  { title: 'React Testing', excerpt: 'Learn React Testing'},
-]
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  { title: "React Testing", excerpt: "Learn React Testing" },
+];
 
 const Home: NextPage = () => {
   return (
@@ -14,16 +14,21 @@ const Home: NextPage = () => {
         <title>Letterway Blog</title>
         <link rel="icon" href="" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {posts.map((posts, index) => (
-          <div>
-            {posts.title}
-            {posts.excerpt}
-          </div>
-        ))}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((posts, index) => (
+            <div>
+              {posts.title}
+              {posts.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relatve top-8"></div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
