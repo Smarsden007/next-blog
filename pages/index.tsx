ccf0feb510bc4559152bc9bb39a0dbc617394a10
Smarsden007 <seannmarsden@gmail.com>
2022-09-02 00:38:21 -0700

    Article Detail and comment section built out bug found on sql query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { PostCard, Categories, PostWidget } from '../components'
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
-
-
-
 export default function Home({ posts }) {
-  console.log(posts)
   return (
     <div className="container mx-auto px-10 mb-8">
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
