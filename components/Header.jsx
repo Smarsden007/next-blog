148c6c6744fb94a980388c632ca224e200aa63b1
Smarsden007 <seannmarsden@gmail.com>
2022-09-03 01:22:05 -0700

    WIP

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import Link from 'next/link';
 import { getCategories } from '../services';
-
+import * as logo from './../public/letterway.png'
 const Header = () => {
   const [categories, setCategories] = useState([]);
 
@@ -14,15 +14,15 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-yellow-400 py-8">
+      <div className="border-b w-full inline-block border-black-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-6xl text-white text-yellow-300	">Letterway</span>
+            <span className="cursor-pointer font-bold text-6xl text-white text-yellow-300">Letterway</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 mt-6 mr-2 font-semibold text-xl cursor-pointer">{category.name}</span></Link>
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 mt-6 mr-2 font-semibold text-yellow-300 text-xl cursor-pointer">{category.name}</span></Link>
           ))}
         </div>
       </div>
