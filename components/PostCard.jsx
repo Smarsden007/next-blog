3a4df976b44a7da35255e353383107dc12158b5e
Smarsden007 <seannmarsden@gmail.com>
2022-09-07 19:02:45 -0700

    Changed colors of buttons and working on accessing author details.

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -18,7 +18,7 @@ const PostCard = ({ post }) => (
       />
     </div>
 
-    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-blue-600 text-3xl font-semibold">
+    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-black-600 text-3xl font-semibold">
       <Link href={`/post/${post.slug}`}>{post.title}</Link>
     </h1>
     <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
@@ -39,7 +39,7 @@ const PostCard = ({ post }) => (
       <div className="font-medium text-gray-700">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 inline mr-2 text-blue-500"
+          className="h-6 w-6 inline mr-2 text-black-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -61,7 +61,7 @@ const PostCard = ({ post }) => (
     </p>
     <div className="text-center">
       <Link href={`/post/${post.slug}`}>
-        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-blue-600 text-lg font-medium rounded text-white px-8 py-3 cursor-pointer">
+        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-black text-lg font-medium rounded text-white px-8 py-3 cursor-pointer">
           Continue Reading
         </span>
       </Link>
